da15e0356bdc55d300e9f717f2631a8fd5d2178f
ivenpoker <nwaforhappiyvan@gmail.com>
2021-03-13 14:44:40 +0100

    Indent all files to 4 spaces (tab)
    Signed-off-by: ivenpoker <nwaforhappiyvan@gmail.com>

--- reactnd-contacts-complete/src/App.js
+++ reactnd-contacts-complete/src/App.js
@@ -1,54 +1,56 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import ListContacts from './ListContacts'
-import CreateContact from './CreateContact'
-import * as ContactsAPI from './utils/ContactsAPI'
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+import ListContacts from './ListContacts';
+import CreateContact from './CreateContact';
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
-    })
-  }
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
+    state = {
+        contacts: []
+    };
 
-    ContactsAPI.remove(contact)
-  }
+    componentDidMount() {
+        ContactsAPI.getAll().then((contacts) => {
+            this.setState({contacts});
+        });
+    }
 
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([ contact ])
-      }))
-    })
-  }
+    removeContact = (contact) => {
+        this.setState((state) => ({
+            contacts: state.contacts.filter((c) => c.id !== contact.id)
+        }));
 
-  render() {
-    return (
-      <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}
-          />
-        )}/>
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )}/>
-      </div>
-    )
-  }
+        ContactsAPI.remove(contact);
+    };
+
+    createContact(contact) {
+        ContactsAPI.create(contact).then(contact => {
+            this.setState(state => ({
+                contacts: state.contacts.concat([contact])
+            }));
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Route exact path='/' render={() => (
+                    <ListContacts
+                        onDeleteContact={this.removeContact}
+                        contacts={this.state.contacts}
+                    />
+                )}/>
+                <Route path='/create' render={({history}) => (
+                    <CreateContact
+                        onCreateContact={(contact) => {
+                            this.createContact(contact);
+                            history.push('/');
+                        }}
+                    />
+                )}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- reactnd-contacts-server/contacts.js
+++ reactnd-contacts-server/contacts.js
@@ -4,67 +4,67 @@ const config = require('./config')
 const db = {}
 
 const defaultData = {
-  contacts: [
-    {
-      id: '242dfef8-529a-11eb-ae93-0242ac130002',
-      name: 'Ryan Florence',
-      email: 'ryan@reacttraining.com',
-      avatarURL: config.origin + '/ryan.jpg'
-    },
-    {
-      id: '299992c6-529a-11eb-ae93-0242ac130002',
-      name: 'Michael Jackson',
-      email: 'michael@reacttraining.com',
-      avatarURL: config.origin + '/michael.jpg'
-    },
-    {
-      id: '307f0b0c-529a-11eb-ae93-0242ac130002',
-      name: 'Tyler McGinnis',
-      email: 'tyler@reacttraining.com',
-      avatarURL: config.origin + '/tyler.jpg'
-    },
-    {
-      name: process.env.FIRST_NAME,
-      email: `${process.env.FIRST_NAME}@dummy.com`,
-      avatarURL: config.origin + '/dummy.jpg'
-    }
-  ]
+    contacts: [
+        {
+            id: '242dfef8-529a-11eb-ae93-0242ac130002',
+            name: 'Ryan Florence',
+            email: 'ryan@reacttraining.com',
+            avatarURL: config.origin + '/ryan.jpg'
+        },
+        {
+            id: '299992c6-529a-11eb-ae93-0242ac130002',
+            name: 'Michael Jackson',
+            email: 'michael@reacttraining.com',
+            avatarURL: config.origin + '/michael.jpg'
+        },
+        {
+            id: '307f0b0c-529a-11eb-ae93-0242ac130002',
+            name: 'Tyler McGinnis',
+            email: 'tyler@reacttraining.com',
+            avatarURL: config.origin + '/tyler.jpg'
+        },
+        {
+            name: process.env.FIRST_NAME,
+            email: `${process.env.FIRST_NAME}@dummy.com`,
+            avatarURL: config.origin + '/dummy.jpg'
+        }
+    ]
 }
 
 const get = (token) => {
-  let data = db[token]
+    let data = db[token]
 
-  if (data == null) {
-    data = db[token] = clone(defaultData)
-  }
+    if (data == null) {
+        data = db[token] = clone(defaultData)
+    }
 
-  return data
+    return data
 }
 
 const add = (token, contact) => {
-  if (!contact.id) {
-    contact.id = Math.random().toString(36).substr(-8)
-  }
+    if (!contact.id) {
+        contact.id = Math.random().toString(36).substr(-8)
+    }
 
-  get(token).contacts.push(contact)
+    get(token).contacts.push(contact)
 
-  return contact
+    return contact
 }
 
 const remove = (token, id) => {
-  const data = get(token)
-  const contact = data.contacts.find(c => c.id === id)
+    const data = get(token)
+    const contact = data.contacts.find(c => c.id === id)
 
-  if (contact) {
-    data.contacts = data.contacts.filter(c => c !== contact)
-  }
+    if (contact) {
+        data.contacts = data.contacts.filter(c => c !== contact)
+    }
 
-  return { contact }
+    return { contact }
 }
 
 module.exports = {
-  get,
-  add,
-  remove,
-  defaultData
+    get,
+    add,
+    remove,
+    defaultData
 }
